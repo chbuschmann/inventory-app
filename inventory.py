cd77349e94473b70504da96e5fe952375cb26f5b
Carl-Henrik Buschmann <chbuschmann@mac.com>
2020-11-09 19:40:00 +0100

    Cleaned code
    Working demo of [2]

--- inventory.py
+++ inventory.py
@@ -6,9 +6,11 @@ sheet = workbook.sheet_by_index(0)
 qrCode = input('Please scan QR code now: ')
 
 columns_to_output = [1, 5, 6, 7]
-for row in range(sheet.nrows):  						# Iterate over every rows of the file
-	if qrCode == sheet.cell_value(row, 5):				# If the user name match the input
-		for col in columns_to_output:					# Iterate over the column to output list
-			print(str(sheet.cell_value(row, col)))  	# Printing the cell at given column in the row.
-		else:
-			print('not found')
+# Iterate over every rows of the file
+for row in range(sheet.nrows):
+	# If the user name match the input
+	if qrCode == sheet.cell_value(row, 5):
+		# Iterate over the column to output list
+		for col in columns_to_output:
+			# Printing the cell at given column in the row.
+			print(str(sheet.cell_value(row, col)))
